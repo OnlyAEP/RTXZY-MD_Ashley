eb5956a1fda76689925de61004a23de55973e790
AEP <91467886+TukangM@users.noreply.github.com>
2024-12-07 09:21:27 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
-global.owner = ['62895331520602']  
-global.mods = ['62895331520602'] 
-global.prems = ['62895331520602']
-global.nameowner = 'Tio'
-global.numberowner = '62895331520602' 
-global.mail = 'support@tioprm.eu.org' 
-global.gc = 'https://chat.whatsapp.com/CP5mV2D9IO730HpBgmkuyi'
-global.instagram = 'https://instagram.com/prm2.0'
-global.wm = '© Tio'
+global.owner = ['6282284960188']  
+global.mods = ['6282284960188'] 
+global.prems = ['6282284960188']
+global.nameowner = 'Hazel'
+global.numberowner = '6282284960188' 
+global.mail = 'tcook@apple.com' 
+global.gc = 'https://google.com'
+global.instagram = 'https://instagram.com/openai'
+global.wm = '© Ashley - OnlyAEPHazel LTD'
 global.wait = '_*Tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
 global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
@@ -17,7 +17,7 @@ global.maxwarn = '3' // Peringatan maksimum
 global.antiporn = true // Auto delete pesan porno (bot harus admin)
 
 //INI WAJIB DI ISI!//
-global.btc = 'YOUR_APIKEY_HERE' 
+global.btc = 'IwISGeLT' 
 //Daftar terlebih dahulu https://api.botcahx.eu.org
 
 //INI OPTIONAL BOLEH DI ISI BOLEH JUGA ENGGA//
